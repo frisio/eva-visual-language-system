8e53626d92edfa64d38322e6eb881d7d11ade450
Harm Jan Luth <harmjanluth@gmail.com>
2018-07-25 11:55:29 +0200

    Added pages FETCH and navigation to them

--- theme/js/eva-vls.js
+++ theme/js/eva-vls.js
@@ -1,10 +1,32 @@
+//import '_redirects';
 import '../../theme/style/theme.scss';
 
-window.onload = () => {
-	const path = location.pathname.split('/')[1];
-	const $navItem = document.querySelector(`main-nav a[href^="/${path}"]`);
-	$navItem && $navItem.classList.add('active');
+const init = () => {
+	var path = location.pathname.split('/');
+	if(!path[1]) path = 'guides';
+	document.querySelector(`main-nav a[href^="/${path[1]}"]`).classList.add('active');
+	document.querySelector(`side-nav.${path[1]}`).style.display = 'block';
+	document.querySelector(`side-nav a[href^="/${path[1]}/${path[2]}"]`).classList.add('active');
+	document.body.classList.add(`page-${path[1]}`);
 
-	document.querySelector(`side-nav.${path}`).style.display = "block";
-	document.body.classList.add(`page-${path}`);
+	const html_page = path.join('_').substr(1);
+	let content = '';
+
+	fetch(`/pages/${html_page}.html`).then((response) => {
+		if (!response.ok) {
+            updateContent(`<p>Page not found..</p>`);
+            return;
+        }
+		response.text().then((text) => {
+		  updateContent(text);
+		});
+	});
+
 }
+
+const updateContent = (text) => {
+	document.querySelector(`content`).innerHTML = text;
+}
+
+window.onload = () =>
+	init();
