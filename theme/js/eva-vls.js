8fb0abe4ed8aa3ff8edc8e759a3cc01a26a0828f
Harm Jan Luth <harmjanluth@gmail.com>
2018-07-25 10:24:11 +0200

    Added navigation

--- theme/js/eva-vls.js
+++ theme/js/eva-vls.js
@@ -2,6 +2,9 @@ import '../../theme/style/theme.scss';
 
 window.onload = () => {
 	const path = location.pathname.split('/')[1];
-	const $navItem = document.querySelector(`main-nav a[href^="${path}"]`);
+	const $navItem = document.querySelector(`main-nav a[href^="/${path}"]`);
 	$navItem && $navItem.classList.add('active');
+
+	document.querySelector(`side-nav.${path}`).style.display = "block";
+	document.body.classList.add(`page-${path}`);
 }
